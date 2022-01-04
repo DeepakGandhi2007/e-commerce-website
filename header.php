387054b60ea66a3c6cac6e7b21fd0b6b6cb8f909
DeepakGandhi2007 <95476025+DeepakGandhi2007@users.noreply.github.com>
2022-01-04 05:59:40 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<title></title>
+	<style type="text/css">
+		
+		.btn
+		{
+			width: 30%;
+			background-color:blue;
+			color: white;
+			margin-left:500px;
+			height:50px;
+			border-radius:30px;
+
+		}
+		.btn:hover
+		{
+			width: 30%;
+			background-color:red;
+			color: white;
+			margin-left:500px;
+			height:50px;
+			border-radius:30px;
+
+		}
+		.btn:active
+		{
+			background-color:greem;
+            box-shadow: 0 9px limegreen;
+            transform: translateY(4px);
+        }
+  
+
+
+	</style>
+</head>
+<body>
+	<h1 style="font-size:50px; text-align: center; font-weight: bold;">E-COMMERCE WEBSITE</h1>
+	<?php require"nav.php"?>
+	   <div class="container-fluid" style="background-image:url(pink.png); color: white; margin-top: 10px;">
+	   	<div class="row">
+	   	 <div class="col-md-6">
+	   			<h1 style="margin-top:80px;margin-left:280px;">Ecommerce Articles</h1>
+	   			<p style="margin-top:80px;margin-left:280px; word-break:10px; word-spacing:1px; word-wrap:unset;">
+	   			Explore our collection of resource articles to become an expert in everything ecommerce.Select a topic below to get started.Explore our collection of resource articles to become an expert in everything ecommerce. Select a topic below to get started.Explore our collection of resource articles to become an expert in everything ecommerce. Select a topic below to get started.</p>
+	   </div>
+	   <div class="col-md-6">
+	   <img src="girl.png" width="58%" height="80%" style="margin-top:50px;float:right; margin-right:250px;">
+	</div>
+      </div>
+  </div>
+ 	   <div>
+ 	   	<br>
+	   	    <h2 style="text-align:center; font-weight:bold;">Choose the Right Ecommerce Platform</h2>
+	   	    <br>
+	   	    <button class="btn">Start Your Free Trial</button>
+	   </div>
+	
+
+
+</body>
+</html>
